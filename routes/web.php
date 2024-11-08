6f4033829a4cdab940cdc8c6912ef3221b1d77a6
notaprogrammervlad <therealvlad3@gmail.com>
2024-11-08 14:59:43 +0800

    Activity 2 - Preline and show students

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Students;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/students', Students\Index::class)->name('students.index');
 });
 
 require __DIR__.'/auth.php';
